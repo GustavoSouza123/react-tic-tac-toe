7acba7093eb8d47b1838622f7fbbe8c7313ce1fe
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-01 12:19:41 -0300

    Board and Square components, handling state in the Square component, Strict Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./assets/App.css";
+import "./assets/styles.css";
+
+import Board from "./components/Board";
 
 function App() {
     return (
         <>
             <h1>Tic-Tac-Toe</h1>
             <p>This is my first <span>REAL</span> React app!</p>
+            <Board />
         </>
     );
 }
