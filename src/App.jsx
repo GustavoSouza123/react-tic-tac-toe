05f9562cedb6b1fb15a07732d3ab463cbdb0fd0d
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-01 11:16:15 -0300

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./assets/App.css";
+
+function App() {
+    return (
+        <>
+            <h1>Tic-Tac-Toe</h1>
+            <p>This is my first <span>REAL</span> React app!</p>
+        </>
+    );
+}
+
+export default App;
