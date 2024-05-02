a84209056228882b520e084a90f0b2e9f5c009b6
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-02 09:33:37 -0300

    add # to app h1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Board from "./components/Board";
 function App() {
     return (
         <>
-            <h1>Tic-Tac-Toe</h1>
+            <h1># Tic-Tac-Toe #</h1>
             <p>This is my first <span>REAL</span> React app!</p>
             <Board />
         </>
